ad36e0073a74a033b9df6ccfc05f57abc32b3e53
kapate <Kaushik.Patel@sas.com>
2017-03-30 14:05:01 -0400

    test

--- 12f_app/src/main/java/com/github/kaushiknc11/app/EnvResource.java
+++ 12f_app/src/main/java/com/github/kaushiknc11/app/EnvResource.java
@@ -1,11 +1,13 @@
 package com.github.kaushiknc11.app;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class EnvResource {
 	@Autowired
